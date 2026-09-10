5681a4b654039635e2eb77aa79816334c635ddfa
Ankit-2563 <ankit123bhavarthe@gmail.com>
2026-09-10 19:24:22 +0530

    Add Bonjour service advertisement for LAN discovery

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -3,15 +3,18 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem!
     private let httpServer = DockHTTPServer()
+    private var bonjourService: NetService?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         setupStatusItem()
         regenerateToken()
         httpServer.start()
+        advertiseBonjour()
     }
 
     func applicationWillTerminate(_ notification: Notification) {
         httpServer.stop()
+        bonjourService?.stop()
     }
 
     private func setupStatusItem() {
@@ -26,8 +29,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         codeItem.isEnabled = false
         menu.addItem(codeItem)
         menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Regenerate Code", action: #selector(regenerateAndRefresh), keyEquivalent: "r"))
+        menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit Boop", action: #selector(quit), keyEquivalent: "q"))
         statusItem.menu = menu
+    }
+
+    @objc private func regenerateAndRefresh() {
+        regenerateToken()
+        rebuildMenu()
     }
 
     private func regenerateToken() {
@@ -36,5 +46,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @objc private func quit() {
         NSApp.terminate(nil)
+    }
+
+    private func advertiseBonjour() {
+        let service = NetService(domain: "local.", type: "_boop._tcp.", name: Host.current().localizedName ?? "Mac", port: Int32(httpServer.port))
+        service.publish()
+        bonjourService = service
     }
 }
