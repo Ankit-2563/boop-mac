caba47f2d6a5591f6af3f0280576b14ebdab353f
Ankit-2563 <ankit123bhavarthe@gmail.com>
2026-09-10 19:24:22 +0530

    Add /apps endpoint to list installed Mac apps

--- HTTPServer.swift
+++ HTTPServer.swift
@@ -16,18 +16,31 @@ final class DockHTTPServer {
             return .ok(.json(body))
         }
 
+        server["/apps"] = { [weak self] request in
+            guard let self, self.isAuthorized(request) else { return .unauthorized(headers: nil) }
+            let apps = AppScanner.scanInstalledApps()
+            let json = apps.map { ["name": $0.name, "path": $0.path, "bundleId": $0.bundleId] }
+            return .ok(.json(json))
+        }
+
         try? server.start(port, forceIPv4: true)
     }
 
     func stop() {
         server.stop()
     }
-}
 
-extension DockHTTPServer {
-    func isAuthorized(_ request: HttpRequest) -> Bool {
+    private func isAuthorized(_ request: HttpRequest) -> Bool {
         guard !pairingToken.isEmpty else { return false }
         let header = request.headers["x-dock-token"]
         return header == pairingToken
+    }
+}
+
+private extension HttpResponse {
+    static func unauthorized(headers: [String: String]?) -> HttpResponse {
+        .raw(401, "Unauthorized", headers, { writer in
+            try? writer.write("unauthorized".data(using: .utf8)!)
+        })
     }
 }
