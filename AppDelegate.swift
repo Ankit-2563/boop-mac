f7963bfd3852e10ed137388a6fe7ae733d4404b9
Ankit-2563 <ankit123bhavarthe@gmail.com>
2026-09-10 19:24:22 +0530

    Security: persist pairing token in UserDefaults across restarts

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -4,10 +4,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem!
     private let httpServer = DockHTTPServer()
     private var bonjourService: NetService?
+    private let tokenKey = "com.boop.pairingToken"
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         setupStatusItem()
-        regenerateToken()
+        loadOrCreateToken()
         httpServer.start()
         advertiseBonjour()
     }
@@ -17,36 +18,53 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         bonjourService?.stop()
     }
 
+    // MARK: - Menu bar UI
+
     private func setupStatusItem() {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         statusItem.button?.image = NSImage(systemSymbolName: "dock.rectangle", accessibilityDescription: "Boop")
+
         rebuildMenu()
     }
 
     private func rebuildMenu() {
         let menu = NSMenu()
+
         let codeItem = NSMenuItem(title: "Pairing code: \(httpServer.pairingToken)", action: nil, keyEquivalent: "")
         codeItem.isEnabled = false
         menu.addItem(codeItem)
+
         menu.addItem(NSMenuItem.separator())
+
         menu.addItem(NSMenuItem(title: "Regenerate Code", action: #selector(regenerateAndRefresh), keyEquivalent: "r"))
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit Boop", action: #selector(quit), keyEquivalent: "q"))
+
         statusItem.menu = menu
     }
 
     @objc private func regenerateAndRefresh() {
-        regenerateToken()
+        let newToken = String(format: "%06d", Int.random(in: 0..<1_000_000))
+        httpServer.pairingToken = newToken
+        UserDefaults.standard.set(newToken, forKey: tokenKey)
         rebuildMenu()
     }
 
-    private func regenerateToken() {
-        httpServer.pairingToken = String(format: "%06d", Int.random(in: 0..<1_000_000))
+    private func loadOrCreateToken() {
+        if let saved = UserDefaults.standard.string(forKey: tokenKey), !saved.isEmpty {
+            httpServer.pairingToken = saved
+        } else {
+            let newToken = String(format: "%06d", Int.random(in: 0..<1_000_000))
+            httpServer.pairingToken = newToken
+            UserDefaults.standard.set(newToken, forKey: tokenKey)
+        }
     }
 
     @objc private func quit() {
         NSApp.terminate(nil)
     }
+
+    // MARK: - Bonjour advertising
 
     private func advertiseBonjour() {
         let service = NetService(domain: "local.", type: "_boop._tcp.", name: Host.current().localizedName ?? "Mac", port: Int32(httpServer.port))
