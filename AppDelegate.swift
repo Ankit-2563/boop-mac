5e4a2fa60f6d0ab610ef7e452b624daa3b6ec58d
Ankit-2563 <ankit123bhavarthe@gmail.com>
2026-09-10 19:24:22 +0530

    Add pairing code generation and display in menu bar

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -6,6 +6,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         setupStatusItem()
+        regenerateToken()
         httpServer.start()
     }
 
@@ -16,10 +17,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private func setupStatusItem() {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         statusItem.button?.image = NSImage(systemSymbolName: "dock.rectangle", accessibilityDescription: "Boop")
+        rebuildMenu()
+    }
 
+    private func rebuildMenu() {
         let menu = NSMenu()
+        let codeItem = NSMenuItem(title: "Pairing code: \(httpServer.pairingToken)", action: nil, keyEquivalent: "")
+        codeItem.isEnabled = false
+        menu.addItem(codeItem)
+        menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit Boop", action: #selector(quit), keyEquivalent: "q"))
         statusItem.menu = menu
+    }
+
+    private func regenerateToken() {
+        httpServer.pairingToken = String(format: "%06d", Int.random(in: 0..<1_000_000))
     }
 
     @objc private func quit() {
